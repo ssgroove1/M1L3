816ed8add9c003462e92784604dc5d224f89ea0d
ss_groove <vsevolodvolk2010@gmail.com>
2025-07-12 17:56:49 +0300

    Сообщение когда игрок заходит выходит бан по ссылке от игрока

--- bot.py
+++ bot.py
@@ -5,6 +5,7 @@ bot = telebot.TeleBot(token)
 
 @bot.message_handler(commands=['start'])
 def start(message):
+    bot.send_message(message.chat.id, message)
     bot.reply_to(message, "Привет! Я бот для управления чатом.")
 
 @bot.message_handler(commands=['ban'])
@@ -15,12 +16,36 @@ def ban_user(message):
         user_id = message.reply_to_message.from_user.id
         user_status = bot.get_chat_member(chat_id, user_id).status 
          # проверка пользователя
-        if user_status == 'administrator' or user_status == 'creator':
+        # if user_status == 'administrator' or user_status == 'creator':
+        if user_status in ('administrator', 'creator'): # Другой варик if
             bot.reply_to(message, "Невозможно забанить администратора.")
         else:
             bot.ban_chat_member(chat_id, user_id) # пользователь с user_id будет забанен в чате с chat_id
             bot.reply_to(message, f"Пользователь @{message.reply_to_message.from_user.username} был забанен.")
     else:
         bot.reply_to(message, "Эта команда должна быть использована в ответ на сообщение пользователя, которого вы хотите забанить.")
+
+
+@bot.message_handler(content_types=['new_chat_members'])
+def make_some(message):
+    bot.send_message(message.chat.id, f'Новый участник @{message.new_chat_members[0].username} зашёл к нам!')
+    bot.approve_chat_join_request(message.chat.id, message.from_user.id)
+    
+
+@bot.message_handler(content_types=['left_chat_member'])
+def make_something(message):
+    bot.send_message(message.chat.id, f'Участник @{message.left_chat_member.username} покинул нас(')
+    bot.decline_chat_join_request(message.chat.id, message.from_user.id)
+
+@bot.message_handler(func=lambda message: "https://" in message.text.lower())
+def ban_message(message):
+    chat_id = message.chat.id
+    user_id = message.from_user.id
+    user_status = bot.get_chat_member(chat_id, user_id).status 
+    if user_status in ('administrator', 'creator'): # Другой варик if
+        bot.reply_to(message, "Невозможно забанить администратора.")
+    else:
+        bot.ban_chat_member(chat_id, user_id) # пользователь с user_id будет забанен в чате с chat_id
+        bot.reply_to(message, f"Пользователь был забанен.")
 
 bot.infinity_polling(none_stop=True)
--- config.py
+++ config.py
@@ -1,1 +1,1 @@
-token = "" # токен для бота
+token = "7562386644:AAG-r215wEN95Ypf2pAQ7h3OX6Fz4CGA8Es" # токен для бота
